201056f184ba077670341d4962a3ccccad0a5010
panicbus <elementalair@gmail.com>
2014-07-11 12:35:55 -0700

    added nav, carousel, carousel content

---
+++ app/assets/javascripts/owl_caro.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+   
+  $("#owl-demo").owlCarousel({
+  
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 500,
+      singleItem : true,
+      autoPlay: true,
+      rewindSpeed: 500,
+      stopOnHover: true
+ 
+  });
+ 
+});
