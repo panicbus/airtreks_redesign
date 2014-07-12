7ed38c2e9009571c1a979f2971fd58cb3c1b295f
panicbus <elementalair@gmail.com>
2014-07-12 01:23:59 -0700

    placed caro control buttons, added specials thumbs, footer with items

--- app/assets/javascripts/owl_caro.js
+++ app/assets/javascripts/owl_caro.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
   $("#owl-demo").owlCarousel({
   
       navigation : true, // Show next and prev buttons
+      navigationText: [
+        "<i class='glyphicon glyphicon-chevron-left'></i>",
+        "<i class='glyphicon glyphicon-chevron-right'></i>"
+      ],
       slideSpeed : 300,
       paginationSpeed : 500,
       singleItem : true,
