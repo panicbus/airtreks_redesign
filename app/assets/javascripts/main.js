bd3dba25ec2c95748fec8bd237eb64ba97138ce3
panicbus <elementalair@gmail.com>
2014-07-15 14:01:46 -0700

    built content nav w slide

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,32 +2,31 @@ $(document).ready(function(){
 
 	var navLink = function(link){
 		$(".view-options").removeClass("current");
+		$(".ready-hiddenitems, .set-hiddenitems, .go-hiddenitems").removeClass("current");
 		$(link).addClass("current")
 	}
 	
-	$("content-default").addClass("current"); // main splash for content section
-
+	$("content-default").addClass("current"); // main splash for content section // no hooked up
 
 	$(".ready").click(function(){
 		navLink(this)
 		$(".ready-div").addClass("current");
+		$(".ready-hiddenitems").slideToggle("current");		
 		
 	});
 
 	$(".set").click(function(event){
 		navLink(this)
 		$(".set-div").addClass("current");
+		$(".set-hiddenitems").slideToggle("current");		
 	});
 
 	$(".go").click(function(event){
 		navLink(this)
 		$(".go-div").addClass("current");
+		$(".go-hiddenitems").slideToggle("current");		
 	});
-
 	
 
-	// if (".rtw101-content").hasClass('current'){
-
-	// }
 
 })
