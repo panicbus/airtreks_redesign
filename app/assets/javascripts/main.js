d9465923f0c77b8624612f0eb5a202a142c2d94d
panicbus <elementalair@gmail.com>
2014-07-15 23:02:18 -0700

    content area, footer updates

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,29 +2,32 @@ $(document).ready(function(){
 
 	var navLink = function(link){
 		$(".view-options").removeClass("current");
-		$(".ready-hiddenitems, .set-hiddenitems, .go-hiddenitems").removeClass("current");
-		$(link).addClass("current")
+		$(".hiddenitems").slideUp();
+		$(".content-default").hide();
+		$(link).addClass("current");
 	}
 	
-	$("content-default").addClass("current"); // main splash for content section // no hooked up
+	// $(".content-default").addClass("current"); // main splash for content section // no hooked up
+
+	// if ($(".hiddenitems") == ))
 
 	$(".ready").click(function(){
 		navLink(this)
-		$(".ready-div").addClass("current");
-		$(".ready-hiddenitems").slideToggle("current");		
+		$(".ready-div").addClass("current"); /// changes the content area to ready
+		$(".ready-hiddenitems").slideDown("current");		
 		
 	});
 
 	$(".set").click(function(event){
 		navLink(this)
-		$(".set-div").addClass("current");
-		$(".set-hiddenitems").slideToggle("current");		
+		$(".set-div").addClass("current"); // changes the content area to set
+		$(".set-hiddenitems").slideDown("current");		
 	});
 
 	$(".go").click(function(event){
 		navLink(this)
-		$(".go-div").addClass("current");
-		$(".go-hiddenitems").slideToggle("current");		
+		$(".go-div").addClass("current"); /// changes the content area to go
+		$(".go-hiddenitems").slideDown("current");		
 	});
 	
 
